ce79b429354fc1d534c40685d875627951fbe3ea
Stephen Chang <stephenwchang@gmail.com>
2019-07-23 14:15:13 -0400

    expandable and collapsible comment threads initial state working

--- client/src/components/CommentItem.js
+++ client/src/components/CommentItem.js
@@ -1,34 +1,68 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
-export class CommentItem extends Component {
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: '100%',
+  },
+  heading: {
+    fontSize: theme.typography.pxToRem(15),
+    fontWeight: theme.typography.fontWeightRegular,
+  },
+}));
+
+
+
+export default function CommentItem(props) {
   // recursively generate comments until no more replies exist
-  renderReplies = () => {
-    if (this.props.replies) {
-      return this.props.replies.map((comment) => (
+
+  const renderReplies = () => {
+    if (props.replies) {
+      return props.replies.map((comment) => (
         <CommentItem key={comment.id} depth={comment.depth} authorName={comment.author.name} replies={comment.replies} score={comment.score} created={comment.created} bodyHtml={comment.body_html} body={comment.body}/>
       ));
     }
   }
+  const classes = useStyles();
 
-  render() {
+  return (
+    <ExpansionPanel>
+    <ExpansionPanelSummary
+      expandIcon={<ExpandMoreIcon/>}
+      aria-controls="panel1a-content"
+      id="panel1a-header"
+    >
+      <Typography className={classes.heading}>
+      <p dangerouslySetInnerHTML={{ __html: props.bodyHtml }}>
+      </p>
+      </Typography>
+    </ExpansionPanelSummary>
+    <ExpansionPanelDetails>
+      <Typography component={'span'} variant={'body2'}>
 
-      return (
-        <div>
-          <div className='commentItem' style={{marginLeft: `${((this.props.depth) * 20).toString()}px` }}>
-            <div style={authorStyle}>{this.props.authorName}</div>
-            <p dangerouslySetInnerHTML={{ __html: this.props.bodyHtml }}>
-            </p>
-          </div>
-          {this.renderReplies()}
-        </div>
+        {renderReplies()}
+      </Typography>
+    </ExpansionPanelDetails>
+  </ExpansionPanel>
+        // <div>
+        //   <div className='commentItem' style={{marginLeft: `${((this.props.depth) * 20).toString()}px` }}>
+        //     <div style={authorStyle}>{this.props.authorName}</div>
+        //     <p dangerouslySetInnerHTML={{ __html: this.props.bodyHtml }}>
+        //     </p>
+        //   </div>
+        //   {this.renderReplies()}
+        // </div>
 
       )
 
-  }
+
 }
 
 const authorStyle = {
   color: "blue"
 }
-
-export default CommentItem;
