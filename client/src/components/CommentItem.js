e9a9ae29bcaf3a6027b131e2e651da2a378c0291
Stephen Chang <stephenwchang@gmail.com>
2019-08-27 22:38:30 -0400

    removed test comment

--- client/src/components/CommentItem.js
+++ client/src/components/CommentItem.js
@@ -42,7 +42,6 @@ export default function CommentItem(props) {
     }}/>
     : null;
 
-    //test comment
 
   return (
     <ExpansionPanel defaultExpanded={expanded} expanded={expansionPanelOpen} TransitionProps={{ unmountOnExit: true }}>
