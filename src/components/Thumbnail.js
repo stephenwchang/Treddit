214f7fd2ebabc4583cf0a090485ba83f0fd401f3
Stephen Chang <stephenwchang@gmail.com>
2019-10-10 17:52:00 -0400

    set placeholder post thumbnail

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -4,7 +4,7 @@ export class Thumbnail extends Component {
   render() {
       return (this.props.thumbnail.includes('jpg')) ? (
         <img src={this.props.thumbnail} alt='thumbnail' style={thumbnailStyle} />
-      ) : <></>
+      ) : <img src='https://309w5s255371fs4df2vftgbl-wpengine.netdna-ssl.com/wp-content/uploads/2017/09/placeholder-300x169.jpg' alt='thumbnail' style={thumbnailStyle}/>
   }
 }
 
