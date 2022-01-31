9f8c82886185eb6e901b9cee156b2ae9bda44a6e
Stephen Chang <stephenwchang@gmail.com>
2022-01-31 17:52:30 -0500

    updated selftext breaking when null, removed author from post title temporarily

--- src/components/SelfText.js
+++ src/components/SelfText.js
@@ -5,8 +5,7 @@ export class SelfText extends Component {
   render() {
     if (this.props.selftext != null) {
       return <p dangerouslySetInnerHTML={{ __html: this.props.selftext }}/>
-
-    }
+    } else return null
   }
 }
 
