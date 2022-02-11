071cae62fcd8fd42b203e4df6a1385c2189c4dcc
Stephen Chang <stephenwchang@gmail.com>
2022-02-11 12:09:13 -0500

    converted comment rendering if statement to ternary, added no comment state and notify user if clicking a post with no comment replies

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -16,11 +16,14 @@ export class Comments extends Component {
   state = {
     comments: [],
     moreCommentsLoaded: false,
+    noComments: false,
   }
 
   renderComments = (id) => {
     r.getSubmission(id).comments.then(result => {
-      this.setState({ comments: Array.from(result) }) // temporary fix due to returned proxy object from snoowrap
+      let resultArray = Array.from(result);
+      resultArray.length === 0 ? this.setState({ noComments: true }) :
+      this.setState({ comments: resultArray }) // temporary fix due to returned proxy object from snoowrap
       console.log(this.state.comments);
     });
   }
@@ -34,8 +37,8 @@ export class Comments extends Component {
   }
 
   render() {
-    if (this.state.comments.length > 0) {
-      return [
+    return (this.state.noComments) ? <div className='loadingComments'>No comments</div> : (this.state.comments.length > 0) ?
+       [
         this.state.comments.map((comment) => (
         <CommentItem
           key={comment.id}
@@ -48,7 +51,7 @@ export class Comments extends Component {
           body={comment.body}
           convertTime={this.props.convertTime}
         />
-      )).filter((x, i) => { return i < 5}) //display only first 5 comments
+      )).filter((x, i) => { return i < 5})  //display only first 5 comments
          ,
         !this.state.moreCommentsLoaded ? <div className='loadAllComments' onClick={this.loadMoreComments}>Show all comments</div>
         :
@@ -64,11 +67,13 @@ export class Comments extends Component {
             body={comment.body}
             convertTime={this.props.convertTime}
           />
-        )).filter((x, i) => { return i > 4})]
-    }
-    else {
-      return <div className='loadingComments'> Loading comments...</div>
-    }
+        )).filter((x, i) => { return i > 4})] :
+     <div className='loadingComments'> Loading comments...</div>
+
+
+    // else {
+    //   return <div className='loadingComments'> Loading comments...</div>
+    // }
   }
 }
 
