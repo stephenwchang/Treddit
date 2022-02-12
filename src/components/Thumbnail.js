9e4dd3b72e215203f1234c705e4a61c037bf93c9
Stephen Chang <stephenwchang@gmail.com>
2022-02-11 19:45:53 -0500

    exchanged placeholder thumbnail for self text posts

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -4,7 +4,7 @@ export class Thumbnail extends Component {
   render() {
       return (this.props.thumbnail.includes('jpg')) ? (
         <a onClick={(e)=> e.stopPropagation()} href={this.props.url} target='_blank' rel='noopener noreferrer'><img className='postThumbnail' src={this.props.thumbnail} alt='thumbnail'/></a>
-      ) : <a onClick={(e)=> e.stopPropagation()} href={this.props.url} target='_blank' rel='noopener noreferrer'><img className='postThumbnail' src='https://d1bdhkmqqz901h.cloudfront.net/x/smart/http://www.dailyjournal.net/wp-content/themes/Newspaper/images/no-thumb/thumbnail.jpg' alt='thumbnail'/></a>
+      ) : <a onClick={(e)=> e.stopPropagation()} href={this.props.url} target='_blank' rel='noopener noreferrer'><img className='postThumbnail' src='https://negony.com/image/cache/placeholder-1000x1000.png' alt='thumbnail'/></a>
   }
 }
 
