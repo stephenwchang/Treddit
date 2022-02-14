4f4845d00f337024d8a7e637e5a787dfebd6b97b
Stephen Chang <stephenwchang@gmail.com>
2022-02-14 17:54:21 -0500

    adjusted popover anchor to position

--- src/components/PopoverHint.js
+++ src/components/PopoverHint.js
@@ -1,33 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import Popover from "@material-ui/core/Popover";
 import Typography from "@material-ui/core/Typography";
 
 export default function SimplePopover(props) {
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const divRef = React.useRef();
+  // const [anchorEl, setAnchorEl] = React.useState(null);
+  // const divRef = React.useRef();
 
 
   // function handleClose() {
   //   setAnchorEl(null)
   // }
 
-  useEffect(() => {
-    setAnchorEl(divRef.current);
-  }, [divRef]);
+  // useEffect(() => {
+  //   setAnchorEl(divRef.current);
+  // }, [divRef]);
 
   let open = props.subNotFound;
   const id = open ? "simple-popover" : undefined;
+  const ref = useRef(null);
+
 
   return (
     <div>
-      <div ref={divRef}></div>
+      <div ref={ref}></div>
       <Popover
         id={id}
+        anchorReference="anchorPosition"
+        anchorPosition={{ top: 80, left: 100 }}
         open={props.subNotFound}
-        anchorEl={anchorEl}
+        // anchorEl={ref}
         onClose={props.handleClose}
         anchorOrigin={{
-          vertical: "bottom",
+          vertical: "center",
           horizontal: "center"
         }}
         transformOrigin={{
