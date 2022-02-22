60876b62e6d494e513383398e9e0c0cf12efd7c6
Stephen Chang <stephenwchang@gmail.com>
2022-02-22 17:48:47 -0500

    changed self-post thumbnail

--- src/components/Thumbnail.js
+++ src/components/Thumbnail.js
@@ -4,7 +4,7 @@ export class Thumbnail extends Component {
   render() {
       return (this.props.thumbnail.includes('jpg')) ? (
         <a onClick={(e)=> e.stopPropagation()} href={this.props.url} target='_blank' rel='noopener noreferrer'><img className='postThumbnail' src={this.props.thumbnail} alt='thumbnail'/></a>
-      ) : <a onClick={(e)=> e.stopPropagation()} href={this.props.url} target='_blank' rel='noopener noreferrer'><img className='postThumbnail' src='https://negony.com/image/cache/placeholder-1000x1000.png' alt='thumbnail'/></a>
+      ) : <a onClick={(e)=> e.stopPropagation()} href={this.props.url} target='_blank' rel='noopener noreferrer'><img className='postThumbnail' src='https://onexl.co/assets/images/course-cover.svg' alt='thumbnail'/></a>
   }
 }
 
